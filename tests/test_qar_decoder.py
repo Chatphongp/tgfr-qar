c2eacc4932cce770d366c6a4d9b72145bd973a92
drano <dog_champ@hotmail.com>
2020-10-24 13:50:46 +0700

    add test

--- tests/test_qar_decoder.py
+++ tests/test_qar_decoder.py
@@ -7,7 +7,13 @@ class TestTwoComplements(unittest.TestCase):
     def _get_int_from_binary_str(self, bin_val):
         return int(bin_val, 2)
 
-    def test_two_complements(self):
+    def test_two_complements_negative_value(self):
         val = self._get_int_from_binary_str("10000000")
         result = qd.twos_complement(val, 8)
         self.assertEqual(result, -128)
+
+    def test_two_complements_positive_value(self):
+
+        val = self._get_int_from_binary_str("01111111")
+        result = qd.twos_complement(val, 8)
+        self.assertEqual(result, 127)
